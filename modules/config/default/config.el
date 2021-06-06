5752e9e4bf187bc09f02f72a26463add83229cf1
Hao Lian <hi@haolian.org>
2021-06-06 14:50:06 -0400

    goodbye IS-MAC

--- modules/config/default/config.el
+++ modules/config/default/config.el
@@ -260,45 +260,45 @@ Continues comments if executed from a commented line. Consults
   (define-key tabulated-list-mode-map "q" #'quit-window))
 
 ;; OS specific fixes
-(when IS-MAC
-  ;; Fix MacOS shift+tab
-  (define-key key-translation-map [S-iso-lefttab] [backtab])
-  ;; Fix conventional OS keys in Emacs
-  (map! "s-`" #'other-frame  ; fix frame-switching
-        ;; fix OS window/frame navigation/manipulation keys
-        "s-w" #'delete-window
-        "s-W" #'delete-frame
-        "s-n" #'+default/new-buffer
-        "s-N" #'make-frame
-        "s-q" (if (daemonp) #'delete-frame #'save-buffers-kill-terminal)
-        "C-s-f" #'toggle-frame-fullscreen
-        ;; Restore somewhat common navigation
-        "s-l" #'goto-line
-        ;; Restore OS undo, save, copy, & paste keys (without cua-mode, because
-        ;; it imposes some other functionality and overhead we don't need)
-        "s-f" #'swiper
-        "s-z" #'undo
-        "s-Z" #'redo
-        "s-c" (if (featurep 'evil) #'evil-yank #'copy-region-as-kill)
-        "s-v" #'yank
-        "s-s" #'save-buffer
-        "s-x" #'execute-extended-command
-        :v "s-x" #'kill-region
-        ;; Buffer-local font scaling
-        "s-+" #'doom/reset-font-size
-        "s-=" #'doom/increase-font-size
-        "s--" #'doom/decrease-font-size
-        ;; Conventional text-editing keys & motions
-        "s-a" #'mark-whole-buffer
-        "s-/" (cmd! (save-excursion (comment-line 1)))
-        :n "s-/" #'evilnc-comment-or-uncomment-lines
-        :v "s-/" #'evilnc-comment-operator
-        :gi  [s-backspace] #'doom/backward-kill-to-bol-and-indent
-        :gi  [s-left]      #'doom/backward-to-bol-or-indent
-        :gi  [s-right]     #'doom/forward-to-last-non-comment-or-eol
-        :gi  [M-backspace] #'backward-kill-word
-        :gi  [M-left]      #'backward-word
-        :gi  [M-right]     #'forward-word))
+;; (when IS-MAC
+;;   ;; Fix MacOS shift+tab
+;;   (define-key key-translation-map [S-iso-lefttab] [backtab])
+;;   ;; Fix conventional OS keys in Emacs
+;;   (map! "s-`" #'other-frame  ; fix frame-switching
+;;         ;; fix OS window/frame navigation/manipulation keys
+;;         "s-w" #'delete-window
+;;         "s-W" #'delete-frame
+;;         "s-n" #'+default/new-buffer
+;;         "s-N" #'make-frame
+;;         "s-q" (if (daemonp) #'delete-frame #'save-buffers-kill-terminal)
+;;         "C-s-f" #'toggle-frame-fullscreen
+;;         ;; Restore somewhat common navigation
+;;         "s-l" #'goto-line
+;;         ;; Restore OS undo, save, copy, & paste keys (without cua-mode, because
+;;         ;; it imposes some other functionality and overhead we don't need)
+;;         "s-f" #'swiper
+;;         "s-z" #'undo
+;;         "s-Z" #'redo
+;;         "s-c" (if (featurep 'evil) #'evil-yank #'copy-region-as-kill)
+;;         "s-v" #'yank
+;;         "s-s" #'save-buffer
+;;         "s-x" #'execute-extended-command
+;;         :v "s-x" #'kill-region
+;;         ;; Buffer-local font scaling
+;;         "s-+" #'doom/reset-font-size
+;;         "s-=" #'doom/increase-font-size
+;;         "s--" #'doom/decrease-font-size
+;;         ;; Conventional text-editing keys & motions
+;;         "s-a" #'mark-whole-buffer
+;;         "s-/" (cmd! (save-excursion (comment-line 1)))
+;;         :n "s-/" #'evilnc-comment-or-uncomment-lines
+;;         :v "s-/" #'evilnc-comment-operator
+;;         :gi  [s-backspace] #'doom/backward-kill-to-bol-and-indent
+;;         :gi  [s-left]      #'doom/backward-to-bol-or-indent
+;;         :gi  [s-right]     #'doom/forward-to-last-non-comment-or-eol
+;;         :gi  [M-backspace] #'backward-kill-word
+;;         :gi  [M-left]      #'backward-word
+;;         :gi  [M-right]     #'forward-word))
 
 
 ;;
